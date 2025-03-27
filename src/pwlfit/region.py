023547ddb0d881fe7b0b537c9b51a54195f01906
David Kirkby <dkirkby@uci.edu>
2025-03-27 06:36:33 -0700

    findRegion updates

--- src/pwlfit/region.py
+++ src/pwlfit/region.py
@@ -19,6 +19,43 @@ class Region:
 def findRegions(fit: pwlfit.fit.FitResult, grid: pwlfit.grid.Grid,
                 inset: int = 4, pad: int = 3, chisq_cut: float = 4,
                 window_size: int = 19, poly_order: int = 1) -> Tuple[float,List[Region]]:
+    """
+    Find regions of the fit where the chisq is above a threshold that can be analyzed independently.
+
+    Parameters
+    ----------
+    fit : FitResult
+        The fit to analyze.
+    grid : Grid
+        The grid used for the fit.
+    inset : int
+        The number of grid points to ignore at the beginning and end of the grid.
+    pad : int
+        The number of grid points to add to the beginning and end of each region.
+    chisq_cut : float
+        The threshold for the chisq above which a region is considered significant.
+        Will be scaled by the median of the smoothed chisq.
+    window_size : int
+        The size of the Savitzky-Golay filter window to smooth the chisq. Must be odd.
+    poly_order : int
+        The order of the Savitzky-Golay filter polynomial to smooth the chisq.
+
+    Returns
+    -------
+    float
+        The median of the smoothed chisq.
+    List[Region]
+        The list of regions where the chisq is above the threshold.
+    """
+    if 2 * inset >= grid.ngrid:
+        raise ValueError(f'Invalid inset value {inset}')
+    if pad < 0 or 2 * pad >= grid.ngrid:
+        raise ValueError(f'Invalid pad value {pad}')
+    if window_size % 2 == 0 or window_size < 0:
+        raise ValueError(f'Invalid window size {window_size} (should be an odd integer > 0)')
+
+    # Inset must be at least big enough for the padding
+    inset = max(inset, pad)
 
     # Smooth chisq with Savitzky-Golay filter
     chisq_smooth = scipy.signal.savgol_filter(fit.chisq, window_size, poly_order)
@@ -27,15 +64,12 @@ def findRegions(fit: pwlfit.fit.FitResult, grid: pwlfit.grid.Grid,
     chisq_median = np.median(chisq_smooth)
     chisq_cut *= chisq_median
 
+    # Build list of consecutive knots where the smooth chisq exceeds the cut,
+    # with padding added
     regions = []
     currentRegion = None
     for i in range(inset, grid.ngrid - 1 - inset):
-        above = False
-        for k in range(grid.breaks[i], grid.breaks[i+1]):
-            if chisq_smooth[k] > chisq_cut:
-                above = True
-                break
-        if above:
+        if np.any(chisq_smooth[grid.breaks[i]:grid.breaks[i+1]] > chisq_cut):
             if currentRegion is None:
                 currentRegion = Region(lo=i - pad, hi=i + pad)
             else:
